f72b5ac1d81e5dae1bd14ec3e02c743051ef28f9
glouglougloup <glouglouglou@outlook.com>
2024-08-14 16:47:37 +0200

    Refactor getProfile method to return a List<MemberRaiderIo> and handle empty results

--- src/main/java/com/deviennefou/devienne_fou_weekly_check/controller/RaiderIOController.java
+++ src/main/java/com/deviennefou/devienne_fou_weekly_check/controller/RaiderIOController.java
@@ -1,5 +1,6 @@
 package com.deviennefou.devienne_fou_weekly_check.controller;
 
+import com.deviennefou.devienne_fou_weekly_check.model.MemberRaiderIo;
 import com.deviennefou.devienne_fou_weekly_check.service.DevienneFouService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -7,16 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 public class RaiderIOController {
     @Autowired
     DevienneFouService devienneFouService;
 
     @GetMapping("/profile")
-    public ResponseEntity<String> getProfile(@RequestParam int rank){
-        devienneFouService.getMembersWithRanks(rank);
+    public ResponseEntity<List<MemberRaiderIo>> getProfile(@RequestParam int rank){
+        Optional<List<MemberRaiderIo>> membersWithRanks = devienneFouService.getMembersWithRanks(rank);
 
-        //TODO replace String with model for front
-        return ResponseEntity.ok("ok");
+        return membersWithRanks.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.noContent().build());
     }
 }
